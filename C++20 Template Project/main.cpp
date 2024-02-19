ac35bccfe6f3aa2da7083d0329ad63beae74b6dd
Jan Dittrich <HonzaDittrich@seznam.cz>
2024-02-19 18:10:29 +0100

    Multithreading

--- C++20 Template Project/main.cpp
+++ C++20 Template Project/main.cpp
@@ -8,110 +8,20 @@
 #include <stack>
 #include <queue>
 #include <list>
-
-
+#include <thread>
 #include <functional>
 
-using namespace std;
 
 
-
-
-// Uniform traversal of containers
-template <typename T> void printCollection (const T& collection){
-
-  auto it = collection.begin();
-  std::cout << "[";
-  while(it != collection.end()){
-    std::cout << " " << *it;
-    ++it;
-  }
-  std::cout << "]" << std::endl;
+void threadFunction(){
+  std::cout << "Im thread function" << std::endl;
 
 }
-
-
-template <typename T, typename K> void printMap (const std::multimap<T,K> & map){
-
-  auto it = map.begin();
-  std::cout << "[";
-  while(it != map.end()){
-    std::cout << " " << it->first << "," << it->second;
-    ++it;
-  }
-  std::cout << " ]" << std::endl;
-
-}
-
-
-
-
-
-// We need to have second parameter that tell us which container is being used. Deque is default one
-template <typename T, typename Container = std::deque<T> > void printStack (std::stack<T, Container> stack){
-
-  // We are working on copy here so it doesnt matter that we are deleting elements from stack
-  std::cout << "[";
-  while(!stack.empty()){
-    T item = stack.top();
-    std::cout << " " << item;
-    stack.pop(); // Removes the top element so the next iteration it will read the next one.
-  }
-  std::cout << " ]" << std::endl;
-
-}
-
-
-template <typename T, typename Container = std::deque<T>> void clearStack (std::stack<T,Container>& stack){
-  while(!stack.empty()){
-    stack.pop(); 
-  }
-}
-
-
-// We need to have second parameter that tell us which container is being used. Deque is default one
-template <typename T, typename Container = std::vector<T> > void printPriorityQueue (std::priority_queue<T, Container> priorityQueue){
-
-  // We are working on copy here so it doesnt matter that we are deleting elements from stack
-  std::cout << "[";
-  while(!priorityQueue.empty()){
-    std::cout << " " << priorityQueue.top();
-    priorityQueue.pop(); // Removes the front element so the next iteration it will read the next one.
-  }
-  std::cout << " ]" << std::endl;
-}
-
-template <typename T, typename Container = std::vector<T>> void clearPriorityQueue (std::priority_queue<T,Container>& priorityQueue){
-  while(!priorityQueue.empty()){
-    priorityQueue.pop(); 
-  }
-}
-
-struct Sum
-{
-  int sum{0};
-  void operator()(int n) { sum += n; }
-};
-
 
 int main()
 {
-
-  std::vector<int> input {5,7,4,2,8,6,1,9,0,3,11,45,6,23};
-  std::vector<int> output {11,22,33};
-  
-  std::cout << "Output size: " << output.size() << std::endl;             // 3
-  std::cout << "Output capacity: " << output.capacity() << std::endl;     // 3
-
-  // Uses whatever space there is. Does not extend the capacity
-  std::transform(std::begin(input),std::end(input),std::begin(output),[](int n){ return n*2;}); 
-  printCollection(output);  // [ 10 14 8 ]   <- 5*2, 7*2, 4*2
-
-  std::cout << "Output size: " << output.size() << std::endl;             // 3
-  std::cout << "Output capacity: " << output.capacity() << std::endl;     // 3
-
-
-}
+  std::thread(threadFunction);  // !! Program will actually crash because when we are going out of this scope the thread object will be destroyed
+}                               // But the thread is still running !!  
 
 
 
--- MultiThreading.md
+++ MultiThreading.md
@@ -5,7 +5,19 @@
   - Object releases ownership in the destructor
   - The resource instance can be moved from one object to another
 
+- Each thread has its own entry point function
+  - When the thread starts, it executes the code in this function
+  - When the function returns the thread ends
+  - The main thread continues to execute its own code
+  - It does not wait for the other threads, unless we explicitly tell it to
 
+- **Thread**
+  - Software thread
+  - For example an object of the C++ std::thread class
+
+- **Task**
+  - Higher level abstraction
+  - Some work that should be performed concurrently
 
 ## Concurrency
 
@@ -25,3 +37,29 @@
 - A program can perform multiple activities at the same time
   - These activities are managed by the operating system
 - Typically there are more software thread than hardware threads
+
+
+## std::thread (C++11)
+
+- The base level of concurrency
+- Rather low level implementation
+- Maps onto a software thread
+- Managed by the operating system
+- Similar to Boost threads, but with some important differences:
+  - No thread cancellation
+  - Different argument passing semantics
+  - Different behavior on thread destruction
+
+### Launching a Thread
+
+- We need to create an std::thread object defined in [\<thread\>](https://en.cppreference.com/w/cpp/thread/thread) header
+- The constructor starts a new execution thread
+- The parent thread will continue its own execution
+- std::thread constructor takes a callable object - Thread's entry point function
+  - The execution thread will invoke this function
+- The entry point function
+  - Can be any callable object
+  - Can not be overloaded
+  - Any return value is ignored
+
+![](Images/startingThread.png) 
