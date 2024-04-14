c00ece93f5e8b4f72e6f91e7cb05630e00881653
Jan Dittrich <HonzaDittrich@seznam.cz>
2024-04-14 22:36:19 +0200

    LeetCode

---
+++ Coding/LeetCode/Array_String/26_RemoveDuplicates.cpp
@@ -0,0 +1,73 @@
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+using namespace std;
+
+// LeetCode 26. Remove Duplicates from Sorted Array 
+/*
+
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
+The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+  Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. 
+  The remaining elements of nums are not important as well as the size of nums.
+  Return k.
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+*/
+
+// The STL algorithm std::unique
+// Runtime 7 ms Memory 21.16 MB
+int removeDuplicatesSTL(vector<int>& nums) {
+  return std::unique(nums.begin(),nums.end()) - nums.begin(); 
+}
+
+// Runtime 5ms Memory 21.02 MB
+int removeDuplicates(vector<int>& nums) {
+  int index=1;
+  for(int i=1; i<nums.size(); i++){
+    if(nums[i]!=nums[i-1]){ // We can do this because its ordered array so the duplicates must be next to each other
+      nums[index]=nums[i];
+      index++;
+    }
+
+  }
+  return index;
+}
+
+
+int main(){
+
+  vector<int> nums {0,1,2,2,3};
+  std::cout << removeDuplicates(nums) << std::endl;
+
+  for(int num: nums){
+    std::cout << num << " ";
+  }
+
+
+  return 0;
+}
+
+
